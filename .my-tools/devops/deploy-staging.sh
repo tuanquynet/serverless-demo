3e19abc05fe490fc87945c90641735482e88f9ce
Quy Tran <tuanquynet@gmail.com>
2019-10-03 17:18:08 +0700

    update dev tools

--- .my-tools/devops/deploy-staging.sh
+++ .my-tools/devops/deploy-staging.sh
@@ -1,4 +1,5 @@
 # AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY are configured in ~/.aws/credentials
+export PATH=$PATH:`pwd`/node_modules/.bin
 
 # test-lambda with set of limit permission
 export AWS_LAMBDA_ROLE=arn:aws:iam::615663043961:role/test-lambda
--- .my-tools/devops/remove-prod.sh
+++ .my-tools/devops/remove-prod.sh
@@ -1,3 +1,4 @@
+export PATH=$PATH:`pwd`/node_modules/.bin
 
 export AWS_LAMBDA_ROLE=arn:aws:iam::615663043961:role/test-lambda
 export AWS_PROFILE=tuanquynet
